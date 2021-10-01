32f904cb178d6f66a911792b666906b1bf99e184
Daniel <danydu1299@gmail.com>
2021-10-01 15:08:02 -0700

    Alphabeth

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Function tha print to alphabet
+ *
+ * Return: Always (0) sucess
+ */
+int main(void)
+{
+	int alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar(alph);
+	}
+	return (0);
+}
