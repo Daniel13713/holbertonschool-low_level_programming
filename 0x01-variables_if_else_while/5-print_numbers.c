8309ad8d45b2d33e2e34abd3528348e04d14804c
Daniel <danydu1299@gmail.com>
2021-10-01 15:51:54 -0700

    print number with putchar

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Function tha print the number from 0 to 9
+ *
+ * Return: Always (0) sucess
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar(10);
+	return (0);
+}
