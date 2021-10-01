08c9b436044d139b66beec8ab18f58e8a1f2b4cd
Daniel <danydu1299@gmail.com>
2021-10-01 13:59:20 -0700

    The last digit using abs

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,22 +10,22 @@
 int main(void)
 {
 	int n;
+	unsigned int lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	unsigned int lastDigit = n % 10;
-
+	lastDigit = abs(n % 10);
 	if (lastDigit > 5)
 	{
 		printf("Last digit of %d is %u and is greater than 5\n", n, lastDigit);
 	}
 	else if (lastDigit < 6 && lastDigit != 0)
 	{
-		printf("Last digit of %d is %u and is less than 6 and not 0\n", n, lastDigit);
+		printf("Last digit of %d is %u and is less than 6 and not 0", n, lastDigit);
 	}
 	else
 	{
-		printf("Last digit of %d id and is 0\n", n);
+		printf("Last digit of %d and is 0\n", n);
 	}
 	return (0);
 }
