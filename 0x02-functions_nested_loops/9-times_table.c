3268eacba72e291962f5997ea23f285192cf7485
Daniel <danydu1299@gmail.com>
2021-10-05 11:13:02 -0700

    solving betty problems

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,15 +20,15 @@ void times_table(void)
 				_putchar(k / 10 + '0');
 				_putchar(k % 10 + '0');
 				if (j != 9)
-                                {
+				{
 					_putchar(44);
-                                        _putchar(32);
-                                }
+					_putchar(32);
+				}
 			}
 			else
 			{
 				if (j != 0)
-				{	
+				{
 					_putchar(32);
 				}
 				_putchar(k % 10 + '0');
