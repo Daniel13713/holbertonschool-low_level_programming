874323b832abb3edf855addb49880ecf54d3f3cb
Daniel <danydu1299@gmail.com>
2021-10-05 09:29:33 -0700

    last digit, i don't undertood becouse need _putchar(num + '0') before return(num) for to work

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,5 +9,18 @@
  */
 int print_last_digit(int n)
 {
-	return (n % 10);
+	int lastDigit;
+
+	lastDigit = n % 10;
+	if (lastDigit < 0)
+	{
+		lastDigit =  -1 * lastDigit;
+		_putchar(lastDigit + '0');
+		return (lastDigit);
+	}
+	else
+	{
+		_putchar(lastDigit + '0');
+		return (lastDigit);
+	}
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,20 +7,18 @@
  */
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	for (i = 0; i <= 2; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j <= 59; j++)
 		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (l = 0; l <= 9; l++)
-				{
-					printf("%d%d:%d%d\n", i, j, k, l);
-					_putchar(10);
-				}
-			}
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(58);
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar(10);
 		}
 	}
 }
