258fb4d3f974d27f2c0bac38886438f5de64648d
Daniel <danydu1299@gmail.com>
2021-10-11 16:01:24 -0700

    point 0, proyect 5

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of a parameter it points
+ *
+ * @n: pointer of number n
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
