a51d3da1911a11e80b52e9053e3ddd4424006688
Daniel <danydu1299@gmail.com>
2021-10-12 16:23:45 -0700

    prints integers of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,33 +4,21 @@
  * print_array - prints the n elements of an array of integers
  *
  * @a: array
- * @b: size of array
+ * @n: size os array
  * Return: Nothing
  */
 
 void print_array(int *a, int n)
 {
-	int i, j; 
-
-	len = _strlen(a);
-
-}
-
-/**
- * _strlen - return the length of a string
- *
- * @s: pointer to string
- * Return: Length of a string
- */
-
-int _strlen(char *s)
-{
 	int i;
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; i < n; i++)
 	{
-		i++;
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
 	}
-	return (i);
+	printf("\n");
 }
