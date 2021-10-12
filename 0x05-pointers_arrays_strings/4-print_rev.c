3380e7d721861251e74322a219d802cb27626603
Daniel <danydu1299@gmail.com>
2021-10-11 18:06:31 -0700

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: String
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i; j >= 0; --j)
+	{
+		_putchar(s[j]);
+	}
+	_putchar(10);
+}
