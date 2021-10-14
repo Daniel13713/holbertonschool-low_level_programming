7ac328b94bc93c0162fdc5604c8977f8012ec1e2
Daniel <danydu1299@gmail.com>
2021-10-14 17:27:51 -0500

    fixing 8

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "Hpllo";
+    char s1[] = "EBG13 (\"ebgngr ol 13 cynprf\", fbzrgvzrf ulcurangrq EBG-13) vf n fvzcyr yrggre fhofgvghgvba pvcure.";
+    char s2[] = "EBG13 (\"ebgngr ol 13 cynprf\", fbzrgvzrf ulcurangrq EBG-13) vf n fvzcyr yrggre fhofgvghgvba pvcure.";
 
     printf("%d\n", _strcmp(s1, s2));
     printf("%d\n", _strcmp(s2, s1));
