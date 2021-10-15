b485f52bd5452bd1322abd5bc2c1efc9542dc0c3
Daniel <danydu1299@gmail.com>
2021-10-15 11:08:10 -0500

    fixing 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,15 @@
 
 char *rot13(char *str)
 {
-	int i;
+	int i, j;
 
 	i = 0;
-	while (str[i] != '\0')
+	j = 0;
+	while (str[j] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		while ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
 		{
-			if (str[i] >= 'n')
+			if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
 			{
 				str[i] = str[i] - 13;
 			}
@@ -24,20 +25,10 @@ char *rot13(char *str)
 			{
 				str[i] = str[i] + 13;
 			}
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			if (str[i] >= 'N')
-			{
-				str[i] = str[i] - 13;
-			}
-			else
-			{
-				str[i] = str[i] + 13;
-			}
-
+			i++;
 		}
 		i++;
+		j++;
 	}
 	return (str);
 }
