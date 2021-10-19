93dce19b6c82ec3453f1f804213cac2d12132378
Daniel <danydu1299@gmail.com>
2021-10-18 23:42:11 -0500

    change value of a pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - change the value of a pointer to string
+ *
+ * @s: pointer to chenge
+ * @to: char
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
