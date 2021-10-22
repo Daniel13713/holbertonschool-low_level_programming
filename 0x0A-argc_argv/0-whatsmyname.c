c9f7bf196abb6a05db7beca30b2d0589d39f49a2
Daniel <danydu1299@gmail.com>
2021-10-22 13:51:03 -0500

    print name of file compile

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints the name of file compile
+ *
+ * @argc: number of atributes on command line
+ * @argv: array of string of the command line
+ * Return: always (0) succes
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
+
