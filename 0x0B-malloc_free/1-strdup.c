c678535f511a6ae33978cff172ec4b4fd2a6de9d
Daniel <danydu1299@gmail.com>
2021-10-26 17:41:38 -0500

    duplicate a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calculate the length of a string
+ *
+ * @s: string
+ * Return: Length of the string s
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		return (i + _strlen_recursion(s + 1));
+	}
+	return (i);
+}
+
+/**
+ * _strdup - duplicate a string with malloc
+ *
+ * @str: string to duplicate
+ * Return: Pointer to string
+ */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int size_str, i = 0;
+
+	if (str == NULL)
+	{
+		return (0);
+	}
+
+	size_str = _strlen_recursion(str);
+	copy = malloc(size_str * sizeof(str));
+
+	if (copy == NULL)
+	{
+		return (0);
+	}
+
+	while (str[i] != '\0')
+	{
+		copy[i] = str[i];
+		i++;
+	}
+	return (copy);
+}
+
