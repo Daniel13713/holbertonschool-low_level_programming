a63934cbbcbfc6488d7b6c66497c172b08ee1ade
Daniel <danydu1299@gmail.com>
2021-10-27 12:56:05 -0500

    coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * count - count how many coins are necesary
+ *
+ * @n: value
+ * @coins: coins to change the value
+ * @nu: number of coins
+ * @i: counter
+ * Return: Integer
+ */
+
+int count(int n, int coins[], int nu, int i)
+{
+	printf("n = %d\n", n);
+	if (n >= coins[i])
+	{
+		return (count(n - coins[i], coins, nu + 1, i));
+	}
+	else if (n < coins[i] && n != 0)
+	{
+		return (count(n, coins, nu, i + 1));
+	}
+	else if (n <= 0)
+	{
+		return (nu);
+	}
+	printf("nu = %d\n", nu);
+	return (0);
+
+
+}
+
+/**
+ * main - prints the minimum number of coins to make
+ * change for an amount of money
+ *
+ * @argc: number of arguments
+ * @argv: arguments passed
+ * Return: Number of coins necesary (integer)
+ */
+
+int main(int argc, char *argv[])
+{
+	int coins[5] = {25, 10, 5, 2, 1};
+	int num = 0;
+
+	if (argc == 2)
+	{
+		if (atoi(argv[1]) <= 0)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+		num = count(atoi(argv[1]), coins, 0, 0);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("num = %d\n", num);
+	return (0);
+}
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,22 +20,28 @@ int **alloc_grid(int width, int height)
 	matrix = malloc(height * sizeof(int *));
 	if (matrix == NULL)
 	{
+		free(matrix);
 		return (0);
 	}
 	for (i = 0; i < height; i++)
 	{
 		matrix[i] = malloc(width * sizeof(int));
+		printf("bol = %d\n", *matrix[i]);
 		if (matrix[i] == NULL)
 		{
+			printf("INIF");
 			for (j = 0; j < width; j++)
 			{
 				free(matrix[j]);
 			}
 			free(matrix);
-			for (j = 0; j < width; j++)
-			{
-				matrix[i][j] = 0;
-			}
+			return (0);
+		}
+		
+		printf("AFTERIF = %d\n", *matrix[i]);
+		for (j = 0; j < width; j++)
+		{
+			matrix[i][j] = 0;
 		}
 	}
 	return (matrix);
