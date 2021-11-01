5d2ab5875899f2d68497a50872752e5a5a5624bd
Daniel <danydu1299@gmail.com>
2021-11-01 18:37:48 -0500

    add malloc to nre dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,6 +15,11 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d = malloc(sizeof(struct dog));
+	if (!d)
+	{
+		free(d);
+		return;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
