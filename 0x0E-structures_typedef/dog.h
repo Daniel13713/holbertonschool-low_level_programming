3dfef7aed3b4fd381fc0d2ab16e32505cc094f7e
Daniel <danydu1299@gmail.com>
2021-11-01 16:55:36 -0500

    struct a dog in .h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - atributtes for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: this struct save the basic information of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
