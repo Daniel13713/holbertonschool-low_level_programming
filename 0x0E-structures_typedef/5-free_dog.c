3b5977e02cbe6e0815cbcc1f5b8c79bee69138e9
Daniel <danydu1299@gmail.com>
2021-11-01 20:11:50 -0500

    free all memory of a structure

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,12 +1,19 @@
 #include "dog.h"
 
 /**
+ * free_dog - free all memory
  *
- *
- *
+ * @d: pointer to structur
+ * Return: Nothing
  */
 
 void free_dog(dog_t *d)
 {
+	if (!d)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
 	free(d);
 }
