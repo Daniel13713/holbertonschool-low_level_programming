746db3e4b3de720287705aed64e63d0800f9e7cc
Daniel <danydu1299@gmail.com>
2021-11-03 12:56:26 -0500

    print name with pointer to function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,29 +1,6 @@
 #include "function_pointers.h"
 
 /**
- * _strlen - return the length of a string
- *
- * @s: pointer to string
- * Return: Length of a string
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	if (!s)
-	{
-		return (0);
-	}
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
  * print_name - use pointer to function f witn argument name
  *
  * @name: argument, name of a person
@@ -33,13 +10,9 @@ int _strlen(char *s)
 
 void print_name(char *name, void (*f)(char *))
 {
-	name = malloc(sizeof(char) * (_strlen(name) + 1));
 	if (!name)
 	{
-		free(name);
-		goto error;
+		return;
 	}
 	f(name);
-error:
-	return;
 }
