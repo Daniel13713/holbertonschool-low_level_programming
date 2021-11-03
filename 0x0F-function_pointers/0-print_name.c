5b203436dc667e5902dfc19c7c918a29b18dcd1e
Daniel <danydu1299@gmail.com>
2021-11-03 12:26:30 -0500

    print a name with pointer to function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - use pointer to function f witn argument name
+ *
+ * @name: argument, name of a person
+ * @f: funtion to pointer
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
