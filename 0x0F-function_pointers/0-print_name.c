98b96c1db91427f31e69bb7d96eca6a16549bdc1
Daniel <danydu1299@gmail.com>
2021-11-03 12:52:51 -0500

    add malloc

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,29 @@
 #include "function_pointers.h"
 
 /**
+ * _strlen - return the length of a string
+ *
+ * @s: pointer to string
+ * Return: Length of a string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	if (!s)
+	{
+		return (0);
+	}
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * print_name - use pointer to function f witn argument name
  *
  * @name: argument, name of a person
@@ -10,5 +33,13 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (!name)
+	{
+		free(name);
+		goto error;
+	}
 	f(name);
+error:
+	return;
 }
