87d45d36ce2ab01b138f772334f930a96df907f6
Daniel <danydu1299@gmail.com>
2021-11-11 19:07:36 -0500

    add a new node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: pointer to string
+ * Return: Length of a string
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+
+	if (s == NULL)
+	{
+		return (0);
+	}
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * add_node - create a new node
+ *
+ * @head: pointer to pointer the struct
+ * @str: string to the structure
+ * Return: length of list_t
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = NULL;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->next = *head;
+	new->len = _strlen(new->str);
+	*head = new;
+	return (*head);
+}
+
