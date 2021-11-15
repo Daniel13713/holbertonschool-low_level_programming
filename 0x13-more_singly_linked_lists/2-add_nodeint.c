f9f21fdd43226aece6bac7f86b6d498c5a889ccb
Daniel <danydu1299@gmail.com>
2021-11-15 16:58:59 -0500

    add node in linked list, fix betty

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - create a new node
+ *
+ * @head: pointer to pointer the struct
+ * @n: integer to the structure
+ * Return: length of list_t
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = NULL;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->next = *head;
+	new->n = n;
+	*head = new;
+	return (*head);
+}
+
