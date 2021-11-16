37b38dcc50600de91a4d0b1a8a957bfd2c6c50a3
Daniel <danydu1299@gmail.com>
2021-11-16 12:29:48 -0500

    case when head is null in free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,9 +9,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current = *head;
+	listint_t *current;
 	listint_t *next_free;
 
+	if (!*head)
+	{
+		return;
+	}
+	current = *head;
 	while (current)
 	{
 		next_free = current->next;
--- 0x13-more_singly_linked_lists/5-main.c
+++ 0x13-more_singly_linked_lists/5-main.c
@@ -24,5 +24,10 @@ int main(void)
     print_listint(head);
     free_listint2(&head);
     printf("%p\n", (void *)head);
+    head = NULL;
+    print_listint(head);
+    free_listint2(&head);
+    printf("%p\n", (void *)head);
+
     return (0);
 }
