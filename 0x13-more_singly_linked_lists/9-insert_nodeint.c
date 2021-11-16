7f09b13c6394e095bbb79c7b18e3d4364f4279da
Daniel <danydu1299@gmail.com>
2021-11-16 14:56:36 -0500

    :hammer: fix insert a new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,44 @@
 #include "lists.h"
 
+
+/**
+ * add_nodeint_end2 - add a new node to the end
+ *
+ * @head: pointer to pointer of structure
+ * @n: integer
+ * Return: pointer to structure (pointer) or null if fail
+ */
+
+listint_t *add_nodeint_end2(listint_t **head, const int n)
+{
+	listint_t *new = NULL;
+	listint_t *last = NULL;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = NULL;
+	new->n = n;
+
+	if (!*head)
+	{
+		*head = new;
+	}
+	else
+	{
+		last = *head;
+		while (last->next)
+		{
+			last = last->next;
+		}
+		last->next = new;
+	}
+	return (*head);
+}
+
 /**
  * listint_len2 - print lenthg of listint_t
  *
@@ -33,32 +72,26 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *new, *current, *temp;
 
-	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (idx == listint_len2(*head))
 	{
-		free(new);
-		return (NULL);
+		add_nodeint_end2(&(*head), n);
+		return (*head);
 	}
+	new = malloc(sizeof(listint_t));
+	if (!new || idx > listint_len2(*head))
+		goto error;
 	if (*head == NULL || head == NULL)
 	{
 		if (idx != 0)
-		{
-			return (NULL);
-		}
-		else
-		{
-			free(new);
-			new->n = n;
-			*head = new;
-			return (*head);
-		}
+			goto error;
+		new->n = n;
+		goto _return;
 	}
 	if (idx == 0 && (*head != NULL || head != NULL))
 	{
 		new->next = *head;
 		new->n = n;
-		*head = new;
-		return (*head);
+		goto _return;
 	}
 	temp = *head;
 	for (i = 0; i < idx || temp == NULL; i++)
@@ -69,5 +102,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->next = temp;
 	new->n = n;
 	current->next = new;
+	return (*head);
+error:
+	free(new);
+	return (NULL);
+_return:
+	*head = new;
 	return (*head);
 }
