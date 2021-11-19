e849b9d1bf7cd1ead4415c890d254dfc31027ef4
Daniel <danydu1299@gmail.com>
2021-11-18 19:12:01 -0500

    print binary number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_binary - functions that print a binary number from decimal
+ *
+ * @n: decimal long int number
+ * Return: Binary number or (0) if fail
+ */
+
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int b = n & 1;
+
+	if (n == 0)
+	{
+		putchar('0');
+		return;
+	}
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	putchar(b + 48);
+}
