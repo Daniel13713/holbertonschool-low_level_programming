1257a65a8515980039f6710d5cb8210eb136c23b
Daniel <danydu1299@gmail.com>
2021-11-23 13:23:08 -0500

    change with STDERR_FILENO and change loop

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -30,30 +30,27 @@ int _strlen_recursion(char *s)
 
 void copy_textfile(const char *filename_one, const char *filename_two)
 {
-	int fd = 0, fd_2, len_write = 0, n_mode = 0;
-	ssize_t fd_read = 0, fd_write = 0, fd_close = 0, fd_close_2;
+	int fd = 0, fd_2, n_mode = 0;
+	ssize_t fd_read = 1024, fd_write = 0, fd_close = 0, fd_close_2;
 	char buff[1024];
 
 	fd = open(filename_one, O_RDONLY, 0777);
 	if (fd == -1)
 		goto error98;
-	n_mode = (S_IRUSR | S_IWUSR) |  (S_IRGRP | S_IWGRP) | S_IROTH;
+	n_mode = (S_IRUSR | S_IWUSR) | (S_IRGRP | S_IWGRP) | S_IROTH;
 	n_mode = 0664;
 	fd_2 = open(filename_two, O_WRONLY | O_CREAT | O_TRUNC, n_mode);
 	if (fd_2 == -1)
 		goto error99;
-	fd_read = read(fd, buff, 1024);
-	while (fd_read)
+	while (fd_read >= 1024)
 	{
-		buff[fd_read] = '\0';
-		len_write = _strlen_recursion(buff);
-		fd_write = write(fd_2, buff, len_write);
+		fd_read = read(fd, buff, 1024);
+		if (fd_read == -1)
+			goto error98;
+		fd_write = write(fd_2, buff, fd_read);
 		if (fd_write == -1)
 			goto error99;
-		fd_read = read(fd, buff, 1024);
 	}
-	if (fd_read == -1)
-		goto error98;
 	fd_close = close(fd);
 	if (fd_close == -1)
 		goto error100;
@@ -62,13 +59,13 @@ void copy_textfile(const char *filename_one, const char *filename_two)
 		goto error100;
 	return;
 error98:
-	dprintf(2, "Error: Can't read from file %s\n", filename_one);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename_one);
 	exit(98);
 error99:
-	dprintf(2, "Error: Can't write to %s\n", filename_two);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_two);
 	exit(99);
 error100:
-	dprintf(2, "Error: Can't close fd %d\n", fd_2);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_2);
 	exit(100);
 }
 
