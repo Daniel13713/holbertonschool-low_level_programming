c713202d08691133667f18a2e13a54bc4188a52c
Daniel <danydu1299@gmail.com>
2021-11-23 11:56:31 -0500

    try with loop to read and write

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+
 /**
  * _strlen_recursion - calculate the length of a string
  *
@@ -26,34 +28,38 @@ int _strlen_recursion(char *s)
  * Return: number of bits that writed, exit 98, 99, or 100 if fail
  */
 
-ssize_t copy_textfile(const char *filename_one, const char *filename_two)
+void copy_textfile(const char *filename_one, const char *filename_two)
 {
 	int fd = 0, fd_2, len_write = 0, n_mode = 0;
-	ssize_t fd_read = 0, fd_write = 0;
+	ssize_t fd_read = 0, fd_write = 0, fd_close = 0, fd_close_2;
 	char buff[1024];
 
 	fd = open(filename_one, O_RDONLY);
 	if (fd == -1)
 		goto error98;
-	fd_read = read(fd, buff, 1024);
-	if (fd_read == -1)
-		goto error98;
-	buff[fd_read] = '\0';
-	close(fd);
-	if (close(fd) == -1)
-		goto error100;
 	n_mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	fd_2 = open(filename_two, O_WRONLY | O_CREAT | O_TRUNC, n_mode);
 	if (fd_2 == -1)
 		goto error99;
-	len_write = _strlen_recursion(buff);
-	fd_write = write(fd_2, buff, len_write);
-	if (fd_write == -1)
-		goto error99;
-	close(fd_2);
-	if (close(fd_2) == -1)
+	fd_read = read(fd, buff, 1024);
+	while (fd_read)
+	{
+		buff[fd_read] = '\0';
+		len_write = _strlen_recursion(buff);
+		fd_write = write(fd_2, buff, len_write);
+		if (fd_write == -1)
+			goto error99;
+		fd_read = read(fd, buff, 1024);
+	}
+	if (fd_read == -1)
+		goto error98;
+	fd_close = close(fd);
+	if (fd_close == -1)
 		goto error100;
-	return (fd_write);
+	fd_close_2 = close(fd_2);
+	if (fd_close_2 == -1)
+		goto error100;
+	return;
 error98:
 	dprintf(2, "Error: Can't read from file %s\n", filename_one);
 	exit(98);
@@ -63,9 +69,6 @@ error99:
 error100:
 	dprintf(2, "Error: Can't close fd %d\n", fd_2);
 	exit(100);
-
-
-
 }
 
 
@@ -78,14 +81,11 @@ error100:
  */
 int main(int ac, char **av)
 {
-	int res;
-
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	res = copy_textfile(av[1], av[2]);
-	printf("-> %i)\n", res);
+	copy_textfile(av[1], av[2]);
 	return (0);
 }
