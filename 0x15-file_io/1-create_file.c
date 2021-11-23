66e645605fd5c8df58be75dddaa93e45231f2a4d
Daniel <danydu1299@gmail.com>
2021-11-23 10:46:23 -0500

    create and trunc when text passed is null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content == NULL)
 	{
-		n = open(filename, O_CREAT, S_IRUSR | S_IWUSR);
+		n = open(filename, O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 		if (n == -1)
 		{
 			goto error;
