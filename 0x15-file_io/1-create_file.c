dbe92001fe9705c0f606a2b2a67f9012f19ce6d1
Daniel <danydu1299@gmail.com>
2021-11-23 10:24:57 -0500

    add O_TRUNC flag when create a new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		goto error;
 	}
-	n = open(filename,  O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	n = open(filename,  O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (n == -1)
 	{
 		goto error;
