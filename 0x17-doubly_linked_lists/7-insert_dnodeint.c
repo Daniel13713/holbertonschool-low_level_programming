c5b7c644b8876458d8b68785905c8e342403fbfe
daniel <danydu1299@gmail.com>
2022-01-07 10:21:30 -0500

    using previus functions

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,11 +39,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->prev = NULL;
 		if (idx == 0)
 		{
-			new->next = *h;
-			if (new->next)
-				(*h)->prev = new;
-			*h = new;
-			return (new);
+			return (add_dnodeint(h, n));
 		}
 		current = *h;
 		for (i = 0; i < idx - 1; i++)
