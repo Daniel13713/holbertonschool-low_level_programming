3c4fae9b3381fa13cb0787744899924af4e43adc
daniel <danydu1299@gmail.com>
2022-01-28 12:51:02 -0500

    don't forget the #ifndef NAME... in main.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef H_TABLE
+#define H_TABLE
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -34,3 +37,5 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
+#endif
