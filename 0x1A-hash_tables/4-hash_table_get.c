c345572ffd299ac3df1e57529ac2915de802c7c2
daniel <danydu1299@gmail.com>
2022-01-28 18:48:37 -0500

    cases to return null

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,6 +14,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *arg_array = NULL;
 	char *value = NULL;
 
+	if (!key || key[0] == '\0' || !ht || !ht->size || !ht->array)
+		return (NULL);
 	/*Obtain the index*/
 	index = key_index((unsigned char *)key, ht->size);
 	/*obtain the value form array*/
