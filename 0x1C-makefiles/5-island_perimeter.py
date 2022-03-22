54425c938c7c50e9ab46ed7e409afbe2be472071
daniel <danydu1299@gmail.com>
2022-03-21 23:24:04 -0500

    try and try

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,17 +12,17 @@ def island_perimeter(grid):
     for i in range(rows):
         j = 0
         for j in range(columns):
-            if grid[i][j] == 0:
-                if j != columns-1 and grid[i][j+1] == 1:
+            if grid[i][j] == 1:
+                if j == columns-1 or grid[i][j+1] == 0:
                     """right"""
                     land += 1
-                if j != 0 and grid[i][j-1] == 1:
+                if j == 0 or grid[i][j-1] == 0:
                     """left"""
                     land += 1
-                if i != 0 and grid[i-1][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
                     """up"""
                     land += 1
-                if i != rows-1 and grid[i+1][j] == 1:
+                if i == rows-1 or grid[i+1][j] == 0:
                     """down"""
                     land += 1
     return land
